187be2c12bc1097c12b37624ea3d2b33b5ec7b66
Anton <anton.estum@gmail.com>
2015-05-15 17:30:04 +0300

    Add & fix some snippets/commands

--- Support/create_files.rb
+++ Support/create_files.rb
@@ -8,7 +8,7 @@ bundle_lib "core_ext/string"
 module CreateFiles
   @extentions = {
     :views       => %w(.erb .slim .haml),
-    :js          => %w(.coffee, .js),
+    :js          => %w(.coffee .js),
     :stylesheets => %w(.scss .less .sass .css),
     :locales     => %w(.yaml .rb),
     :controllers => %w(.rb)
@@ -25,41 +25,41 @@ module CreateFiles
     :initializer => 'config/initializers',
     :locales     => 'config/locale'
   }
-  
+
   def self.create_file(filename, type, *argv)
     @argv     = [argv].flatten.first
     @filename = filename
     @type     = type
-    
+
     @filename ||= "untitled"
     # @type ||= ".rb"
-    
+
     @current_path        = ENV['TM_FILEPATH']
     @current_dir         = ENV['TM_DIRECTORY']
     @current_project_dir = ENV['TM_PROJECT_DIRECTORY']
     @extname             = set_extname
     @dir                 = @argv[:dir].nil? ? set_path : [@pathes[@type], @argv[:dir]].join('/')
     @abs_dir             = File.join(@current_project_dir, @dir)
-    
+
     Dir.mkpath(@abs_dir) if !Dir.exists?(@abs_dir)
-    
+
     @fullpath = File.join(@abs_dir, "#{@filename}#{@extname}")
-    
+
     file = !File.exist?(@fullpath) ? File.new(@fullpath, 'w+') : File.open(@fullpath, 'w+')
     TextMateRailsFile.new(file)
   end
-  
+
   def self.set_extname
     current_ext = File.extname(@current_path)
-    
+
     return '.rb'       if @extentions[@type].nil?
     return current_ext if current_ext.in?(@extentions[@type])
     @extentions[@type].first
   end
-  
+
   def self.set_path
     current_path  = @current_dir - "#{@current_project_dir}/"
-    selected_path = get_selected_path    
+    selected_path = get_selected_path
     path          = current_path
 
     if !@pathes[@type].nil?
@@ -73,15 +73,15 @@ module CreateFiles
     elsif selected?
       path = selected_path
     end
-      
+
     path
   end
-  
-  
+
+
   def self.is_matches?(path)
     path.start_with?(@pathes[@type])
   end
-  
+
   def self.get_selected_path
     selected_file = ENV['TM_SELECTED_FILE']
     if selected?
@@ -90,7 +90,7 @@ module CreateFiles
     end
     selected_file
   end
-  
+
   def self.selected?
     !ENV['TM_SELECTED_FILE'].nil? && ENV['TM_SELECTED_FILE'].start_with?(@current_project_dir)
   end
@@ -100,9 +100,9 @@ end
 class TextMateRailsFile
   def initialize(file, *options)
     @file = file
-    @path = File.absolute_path @file.path    
+    @path = File.absolute_path @file.path
   end
-  
+
   def open
     %x{open txmt://open/?url=file://#{e_sh @path}}
     self
