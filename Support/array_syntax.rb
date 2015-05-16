496e471f74eae31aeba1c055a36d7a909e628c66
Anton <anton.estum@gmail.com>
2015-05-16 19:33:53 +0300

    Super-working array-syntax

--- Support/array_syntax.rb
+++ Support/array_syntax.rb
@@ -1,28 +1,103 @@
-require File.join(ENV['TM_BUNDLE_SUPPORT'], 'lib/core_ext/object/blank.rb')
+require "forwardable"
+require File.join(ENV['TM_SUPPORT_PATH'], 'lib/current_word')
+require File.join(ENV['TM_BUNDLE_SUPPORT'], 'lib/core_ext/object/blank')
 require File.join(ENV['TM_BUNDLE_SUPPORT'], 'lib/core_ext/enumerable')
 
 class String
   CLOSING_MAP = { "[" => "]", "(" => ")", "{" => "}" }.freeze
-  def closing; CLOSING_MAP[chr] || self; end
+  def closing
+    CLOSING_MAP[chr] || self
+  end
 end
 
-class ArraySyntax
-  attr_accessor :input_text
+module ArraySyntax
+  class Toggle
+    attr_accessor :input, :source, :swapped, :output
 
-  def self.call(value)
-    new.call(value)
+    def initialize(input)
+      @input   = input
+      @source  = selected? ? input : get_array_on_cursor
+      @swapped = swap_syntax_of(source)     if source.present?
+      @output  = if swapped.present?
+        input.dup.tap do |s|
+          if selected?
+            s.replace(swapped)
+          else
+            s.slice!(@from...@to)
+            s.insert(@from, swapped)
+          end
+        end
+      end
+      yield(@output)
+    end
+
+    private
+
+    def get_array_on_cursor
+      left  = Word.current_word(/^(.+?(?:\p{Ps}[wi]%|\[(?!\b)))/i, :left)
+      close = left[/(?<=^%[wi])\p{Ps}|^\[/i].to_s.closing
+      right = Word.current_word(/^(.+?(?:#{close.prepend(?\\)}))/, :right) if close.present?
+      return unless right.present?
+      @from = (ENV['TM_COLUMN_NUMBER'].to_i - left.length).pred
+      @to   = (ENV['TM_COLUMN_NUMBER'].to_i + right.length).pred
+      [left, right].join
+    end
+
+    def literalizer_for(items)
+      klass = [SymbolLiteral, StringLiteral].
+        find { |o| items.all? { |e| e =~ o.pattern } }.
+        presence || NullLiteral
+      klass.new
+    end
+
+    def swap_syntax_of(part)
+      if /(?<=^\[)(.+?)(?=\]$)/ =~ part
+        items = scan_items($1)
+        literalizer_for(items).render_with(items)
+      else
+        if /^%(w|i)\p{Ps} *(.*?) *?\p{Pe}$/i =~ part
+          is_string = $1.downcase == "w"
+          s = $2.split(/(?<!\\)\s+/).map { |v| toggle_interpolation(v, is_string) }.join(", ")
+          "[#{s}]"
+        end
+      end
+    end
+
+    def scan_items(part)
+      part.split(?,).
+        slice_when {|l,r| r.lstrip =~ /\A:?(["']?).+?(?!\1)\z/ || l.rstrip =~ /\A[^'"]+?['"]?\z/ }.
+        map { |a| a.join(?,).strip.gsub(' ', '\\ ') }
+    end
+
+    def toggle_interpolation(value, is_string)
+      if value.start_with?('#')
+        value[/(?<=#\{)(.*?)(?=\})/, 1]
+      else
+        (is_string ? %Q["#{value.gsub(/\\\s/, ' ')}"] : value.to_sym.inspect)
+      end
+    end
+
+    def selected?
+      ENV['TM_SELECTED_TEXT'].present?
+    end
+
+    def line?(n)
+      ENV['TM_LINE_NUMBER'].to_i == n.next
+    end
+
+    def current_line
+      ENV['TM_CURRENT_LINE']
+    end
   end
 
-  def call(value)
-    @input_text = value
-    selected? ? print_for_selection! : print_for_line!
-  end
+  class Literalizer
+    extend Forwardable
+    class << self; attr_accessor(:chr, :pattern); end
+    delegate [:chr, :pattern] => "self.class"
 
-  SwapToLiteral = Struct.new(:chr, :pattern) do
-    def call(items)
-      return unless items.all? { |e| e =~ pattern }
-      @converted ||= items.map { |e| process(e) }
-      "%#{@_chr || chr}(#{@converted * ' '})"
+    def render_with(items)
+      normalized = items.map { |e| process(e) }
+      "%#{@_chr || chr}(#{normalized * ' '})"
     end
 
     def process(e)
@@ -35,83 +110,18 @@ class ArraySyntax
     end
   end
 
-  class SymbolLiteral < SwapToLiteral
-    def initialize; super(:i, /^(:|(?!'|"))(.+?)$/); end
-  end
-
-  class StringLiteral < SwapToLiteral
-    def initialize; super(:w, /^('|"|(?!:))(.+?)\1$/); end
-  end
-
-  protected
-
-  def all_items_matches?(items, pattern)
-    items.all? { |i| pattern === i }
-  end
-
-  def ary_match(part)
-    if /(?<=^\[)(.+?)(?=\]$)/ =~ part
-      ary = scan_items($1)
-      SymbolLiteral.new.(ary) || StringLiteral.new.(ary)
-    else
-      if part =~ /^%(w|i)\p{Ps} *(.*?) *?\p{Pe}$/i
-        is_string = $1.downcase == "w"
-        s = $2.split(/(?<!\\)\s+/).map { |v| toggle_interpolation(v, is_string) }.join(", ")
-        "[#{s}]"
-      end
+  class SymbolLiteral < Literalizer
+    @chr, @pattern = :i, /^(:|(?!'|"))(.+?)$/
+    def process(e)
+      super(e)[/^(['"]?)(.*)\1$/, 2]
     end
   end
 
-  def scan_items(part)
-    part.split(?,).
-      slice_when {|l,r| /\A\s*:?(["']).+?(?!\1)\z/ =~ r || /\A.+?['"]\s*\z/ =~ l }.
-      map {|a| a.join(?,).strip.gsub(' ', '\\ ') }
+  class StringLiteral < Literalizer
+    @chr, @pattern = :w, /^('|"|(?!:))(.+?)\1$/
   end
 
-  def toggle_interpolation(value, is_string)
-    if value.start_with?('#')
-      value[/(?<=#\{)(.*?)(?=\})/, 1]
-    else
-      (is_string ? %Q["#{value.gsub(/\\\s/, ' ')}"] : ":#{value}")
-    end
-  end
-
-  ARRAY_LEFT = /.*(%[wi]\p{Ps}|(?<!%[wi])\[)(.*)$/i
-  ARRAY_RIGHT = /^(.+)(?=\p{Pe})\p{Pe}/
-
-  def ary_of_cur(line, cur)
-    left  = line[0, cur].scan(ARRAY_LEFT).flatten
-    close = left[0] =~ /^%[wi](\p{Ps})/i ? $1.closing.prepend(?\\) : '\]'
-    right = line[cur..-1].scan(/^(.*?)(#{close})/).flatten
-    left.join + right.join
-  end
-
-  def print_for_line!
-    input_text.lines.each_with_index do |input_line, n|
-      output = if line?(n)
-        matched_array = ary_of_cur(current_line, ENV['TM_LINE_INDEX'].to_i)
-        if (matched_array.length > 0) && (output = ary_match(matched_array))
-          input_line.sub(matched_array, output)
-        end
-      end
-      print(output.presence || input_line)
-    end
-  end
-
-  def print_for_selection!
-    output = ary_match(input_text)
-    print(output.presence || input_text)
-  end
-
-  def selected?
-    ENV['TM_SELECTED_TEXT'].present?
-  end
-
-  def line?(n)
-    ENV['TM_LINE_NUMBER'].to_i == n.next
-  end
-
-  def current_line
-    ENV['TM_CURRENT_LINE']
+  class NullLiteral < Literalizer
+    def render_with(*); end
   end
 end
